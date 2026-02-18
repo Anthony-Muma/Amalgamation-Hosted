c700fbd3f53dac4401b2c7148ffa5aabbd5cad9d
Existenial <150177160+Existenial@users.noreply.github.com>
2026-02-17 18:03:42 -0700

    Created createLobby

--- app/lobby-system/lobbyStore.js
+++ app/lobby-system/lobbyStore.js
@@ -1,6 +1,8 @@
-const { randomUUID } = require("crypto")
+// Required
+const { Game } = require("./../server-side-game/game");
+const { randomUUID } = require("crypto");
 
-// In memory
+// Internal variables
 const lobbies = new Map();
 // lobbyId -> { hostId: string, players: Set<socketId>, Game }
 
@@ -8,11 +10,31 @@ function generateLobbyCode() {
     return randomUUID().replace(/-/g, "").slice(0, 6).toUpperCase();
 }
 
+/**
+ * Creates a lobby and stores it, ready for others to connect to.
+ * @param {string} hostSocketId 
+ * @returns lobbyId The lobbyId of the created lobby
+ */
 function createLobby(hostSocketId) {
+
+    // Create the lobbyId
+    const lobbyId = generateLobbyCode();
+
+    // Create the lobby object and store it.
+    lobbies.set(lobbyId, {
+        hostId: hostSocketId,
+        players: new Set([hostSocketId]),
+        game: new Game()
+    });
+
+    // Return
+    return lobbyId;
 
 }
 
 function joinLobby(lobbyId, socketId) {
+
+
 
 }
 

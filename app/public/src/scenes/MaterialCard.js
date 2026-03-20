ffd024a6a4bfe6ff8c58f65339e72018a2c05989
Anthony Muma <amuma917@mtroyal.ca>
2026-03-20 00:13:41 -0600

    added: various prefab scene for working on card designs

---
+++ app/public/src/scenes/MaterialCard.js
@@ -0,0 +1,123 @@
+
+// You can write more code here
+
+/* START OF COMPILED CODE */
+
+/* START-USER-IMPORTS */
+/* END-USER-IMPORTS */
+
+export default class MaterialCard extends Phaser.GameObjects.Container {
+
+	constructor(scene, x, y) {
+		super(scene, x ?? 0, y ?? -152);
+
+		// cardFront
+		const cardFront = scene.add.image(0, 0, "200x280-Bright-White-Card");
+		cardFront.scaleX = 0.8;
+		cardFront.scaleY = 0.8;
+		cardFront.tintTopLeft = 9803157;
+		cardFront.tintTopRight = 9803157;
+		cardFront.tintBottomLeft = 0;
+		cardFront.tintBottomRight = 0;
+		this.add(cardFront);
+
+		// mainImage
+		const mainImage = scene.add.image(0, -32, "200x200-Pillow");
+		mainImage.scaleX = 0.6;
+		mainImage.scaleY = 0.6;
+		this.add(mainImage);
+
+		// shadowFx
+		mainImage.preFX.addShadow(0, 0, 0.1, 1, 0, 6, 1);
+
+		// title
+		const title = scene.add.text(0, -88, "", {});
+		title.setOrigin(0.5, 0.5);
+		title.text = "PILLOW";
+		title.setStyle({ "fontFamily": "Eczar-Bold", "fontSize": "18px", "stroke": "#000000ff", "strokeThickness": 5, "shadow.blur": 12, "shadow.stroke": true, "resolution": 3 });
+		this.add(title);
+
+		// defenseSymbol
+		const defenseSymbol = scene.add.image(40, 40, "100x100-Blue-Shield");
+		defenseSymbol.scaleX = 0.8;
+		defenseSymbol.scaleY = 0.8;
+		this.add(defenseSymbol);
+
+		// powerSymbol
+		const powerSymbol = scene.add.image(-40, 40, "100x100-Power");
+		powerSymbol.scaleX = 0.8;
+		powerSymbol.scaleY = 0.8;
+		this.add(powerSymbol);
+
+		// powerText
+		const powerText = scene.add.text(-40, 40, "", {});
+		powerText.setOrigin(0.5, 0.5);
+		powerText.text = "3";
+		powerText.setStyle({ "color": "#ffffffff", "fontFamily": "Eczar-Bold", "fontSize": "32px", "stroke": "#000000ff", "strokeThickness": 5, "shadow.blur": 12, "shadow.stroke": true, "resolution": 3 });
+		this.add(powerText);
+
+		// defenseText
+		const defenseText = scene.add.text(40, 40, "", {});
+		defenseText.setOrigin(0.5, 0.5);
+		defenseText.text = "10";
+		defenseText.setStyle({ "color": "#ffffffff", "fontFamily": "Eczar-Bold", "fontSize": "32px", "stroke": "#000000ff", "strokeThickness": 5, "shadow.blur": 12, "shadow.stroke": true, "resolution": 3 });
+		this.add(defenseText);
+
+		// orText
+		const orText = scene.add.text(0, 64, "", {});
+		orText.setOrigin(0.5, 0.5);
+		orText.text = "OR";
+		orText.setStyle({ "fontFamily": "Eczar-Bold", "fontSize": "18px", "stroke": "#000000ff", "strokeThickness": 5, "shadow.blur": 12, "shadow.stroke": true, "resolution": 3 });
+		this.add(orText);
+
+		// energyText
+		const energyText = scene.add.text(0, 88, "", {});
+		energyText.setOrigin(0.5, 0.5);
+		energyText.text = "[10 ENG]";
+		energyText.setStyle({ "color": "#ffffffff", "fontFamily": "Eczar-Bold", "stroke": "#000000ff", "strokeThickness": 5, "shadow.blur": 12, "shadow.stroke": true, "resolution": 3 });
+		this.add(energyText);
+
+		this.cardFront = cardFront;
+		this.mainImage = mainImage;
+		this.title = title;
+		this.defenseSymbol = defenseSymbol;
+		this.powerSymbol = powerSymbol;
+		this.powerText = powerText;
+		this.defenseText = defenseText;
+		this.orText = orText;
+		this.energyText = energyText;
+
+		/* START-USER-CTR-CODE */
+		// Write your code here.
+		/* END-USER-CTR-CODE */
+	}
+
+	/** @type {Phaser.GameObjects.Image} */
+	cardFront;
+	/** @type {Phaser.GameObjects.Image} */
+	mainImage;
+	/** @type {Phaser.GameObjects.Text} */
+	title;
+	/** @type {Phaser.GameObjects.Image} */
+	defenseSymbol;
+	/** @type {Phaser.GameObjects.Image} */
+	powerSymbol;
+	/** @type {Phaser.GameObjects.Text} */
+	powerText;
+	/** @type {Phaser.GameObjects.Text} */
+	defenseText;
+	/** @type {Phaser.GameObjects.Text} */
+	orText;
+	/** @type {Phaser.GameObjects.Text} */
+	energyText;
+
+	/* START-USER-CODE */
+
+	// Write your code here.
+
+	/* END-USER-CODE */
+}
+
+/* END OF COMPILED CODE */
+
+// You can write more code here

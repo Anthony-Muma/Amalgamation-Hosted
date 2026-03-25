9f35ac9706f9bd3b0139471fca3049df9b2bcd67
Anthony Muma <amuma917@mtroyal.ca>
2026-03-24 18:24:08 -0600

    wip: testing card enemy redesign

--- app/public/src/scenes/Level.js
+++ app/public/src/scenes/Level.js
@@ -26,7 +26,7 @@ export default class Level extends Phaser.Scene {
 		wooden_Table_Background.tintTopRight = 0;
 
 		// drawDeck1
-		const drawDeck1 = this.add.image(1360, -16, "Card back");
+		const drawDeck1 = this.add.image(1344, -48, "Card back");
 		drawDeck1.setInteractive(new Phaser.Geom.Rectangle(0, 0, 600, 840), Phaser.Geom.Rectangle.Contains);
 		drawDeck1.scaleX = 0.25;
 		drawDeck1.scaleY = 0.25;
@@ -38,16 +38,10 @@ export default class Level extends Phaser.Scene {
 		drawDeck2.scaleY = 0.25;
 
 		// _200x280_Vital_Soul_Card
-		this.add.image(144, 256, "200x280-Vital-Soul-Card");
-
-		// energyPoolText
-		const energyPoolText = this.add.text(144, 256, "", {});
-		energyPoolText.setOrigin(0.5, 0.5);
-		energyPoolText.text = "0";
-		energyPoolText.setStyle({ "color": "#1bc540", "fontFamily": "Eczar-Bold", "fontSize": "64px", "stroke": "#000000ff", "strokeThickness": 10 });
+		this.add.image(-304, 288, "200x280-Vital-Soul-Card");
 
 		// placementsLeftText
-		const placementsLeftText = this.add.text(1168, 352, "", {});
+		const placementsLeftText = this.add.text(144, 448, "", {});
 		placementsLeftText.setOrigin(0.5, 0.5);
 		placementsLeftText.text = "-/-";
 		placementsLeftText.setStyle({ "align": "center", "color": "#ffffffff", "fontFamily": "Eczar-Bold", "fontSize": "64px", "stroke": "#000000ff", "strokeThickness": 24, "resolution": 3 });
@@ -65,22 +59,46 @@ export default class Level extends Phaser.Scene {
 		text.text = "Placement\nCounter";
 		text.setStyle({ "align": "center", "color": "#ffffffff", "fontFamily": "Eczar-Bold", "fontSize": "32px", "stroke": "#000000ff", "strokeThickness": 24, "resolution": 3 });
 
-		// text_1
-		const text_1 = this.add.text(1136, 496, "", {});
-		text_1.setOrigin(0.5, 0.5);
-		text_1.text = "'Space'-End Turn";
-		text_1.setStyle({ "align": "center", "color": "#ffffffff", "fontFamily": "Eczar-Bold", "fontSize": "32px", "stroke": "#000000ff", "strokeThickness": 24, "resolution": 3 });
+		// _200x200_Energy_Crystals_3X
+		const _200x200_Energy_Crystals_3X = this.add.image(160, 272, "200x200-Energy-Crystals-3X");
+		_200x200_Energy_Crystals_3X.scaleX = 1.5;
+		_200x200_Energy_Crystals_3X.scaleY = 1.5;
+
+		// energyPoolText
+		const energyPoolText = this.add.text(144, 288, "", {});
+		energyPoolText.setOrigin(0.5, 0.5);
+		energyPoolText.text = "0";
+		energyPoolText.setStyle({ "color": "#ffffffff", "fontFamily": "Eczar-Bold", "fontSize": "64px", "stroke": "#000000ff", "strokeThickness": 10 });
+
+		// _200x280_Character_Card
+		const _200x280_Character_Card = this.add.image(640, 608, "200x280-Character-Card");
+		_200x280_Character_Card.scaleX = 0.8;
+		_200x280_Character_Card.scaleY = 0.8;
+
+		// _200x280_Character_Card_1
+		const _200x280_Character_Card_1 = this.add.image(1872, 320, "200x280-Character-Card");
+		_200x280_Character_Card_1.scaleX = 1.5;
+		_200x280_Character_Card_1.scaleY = 3.063370076576077;
+
+		// _200x280_Character_Card_2
+		const _200x280_Character_Card_2 = this.add.image(640, 368, "200x280-Character-Card");
+		_200x280_Character_Card_2.scaleX = 0.8;
+		_200x280_Character_Card_2.scaleY = 0.8;
+
+		// _200x280_Character_Card_3
+		const _200x280_Character_Card_3 = this.add.image(640, 128, "200x280-Character-Card");
+		_200x280_Character_Card_3.scaleX = 0.8;
+		_200x280_Character_Card_3.scaleY = 0.8;
 
 		// lists
 		const list = [];
 
 		this.drawDeck1 = drawDeck1;
 		this.drawDeck2 = drawDeck2;
-		this.energyPoolText = energyPoolText;
 		this.placementsLeftText = placementsLeftText;
 		this.drawDeck_2 = drawDeck_2;
 		this.text = text;
-		this.text_1 = text_1;
+		this.energyPoolText = energyPoolText;
 		this.list = list;
 
 		this.events.emit("scene-awake");
@@ -91,15 +109,13 @@ export default class Level extends Phaser.Scene {
 	/** @type {Phaser.GameObjects.Image} */
 	drawDeck2;
 	/** @type {Phaser.GameObjects.Text} */
-	energyPoolText;
-	/** @type {Phaser.GameObjects.Text} */
 	placementsLeftText;
 	/** @type {Phaser.GameObjects.Image} */
 	drawDeck_2;
 	/** @type {Phaser.GameObjects.Text} */
 	text;
 	/** @type {Phaser.GameObjects.Text} */
-	text_1;
+	energyPoolText;
 	/** @type {Array<any>} */
 	list;
 
